f2e827cc3c17d92208ca9036aeb6ec28cf738e5d
michelle moyer <michelle.moyer@remote.com>
2022-07-27 22:06:25 -0400

    forgot to commit after 4. this is half thru 5

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -5,6 +5,8 @@ defmodule Rumbl.Accounts do
   alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
+  # talks to actual database
+  # no changes actually happen here, they happen in user
   def get_user(id) do
     Repo.get(User, id)
   end
@@ -19,6 +21,26 @@ defmodule Rumbl.Accounts do
 
   def list_users do
     Repo.all(User)
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+     |> User.changeset(attrs)
+     |> Repo.insert()
+  end
+
+  def change_registration(%User{} = user, params) do
+    User.registration_changeset(user, params)
+  end
+
+  def register_user(attrs \\ %{}) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
   end
 
 end
--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -2,14 +2,54 @@ defmodule RumblWeb.UserController do
   use RumblWeb, :controller
 
   alias Rumbl.Accounts
-
+  alias Rumbl.Accounts.User
+  # this basically says make sure we authenicate on these two kind of requests
+  plug :authenticate when action in [:index, :show]
+# controller defines all the CRUD options we set in router (get, delete, post, put)
+# calls on accounts context's to get data from it
+# then calls render for template
   def index(conn, _params) do
-    users = Accounts.list_users()
-    render(conn, "index.html", users: users)
+      users = Accounts.list_users()
+      render(conn, "index.html", users: users)
   end
 
   def show(conn, %{"id" => id}) do
     user = Accounts.get_user(id)
     render(conn, "show.html", user: user)
+  end
+
+  def new(conn, _params) do
+    changeset = Accounts.change_registration(%User{}, %{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    case Accounts.register_user(user_params) do
+    {:ok, user} ->
+      conn
+      |> put_flash(:info, "#{user.name} created!")
+      |> redirect(to: Routes.user_path(conn, :index))
+    {:error, %Ecto.Changeset{} = changeset} ->
+      # this is leterally calling the tempplate and passing in the failed validations
+      render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
+    end
   end
 end
