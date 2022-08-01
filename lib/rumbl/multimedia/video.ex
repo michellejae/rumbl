3a2fb6279ac2a449b1a19efce23dd508d078d9b8
michelle moyer <michelle.moyer@remote.com>
2022-07-31 21:56:45 -0400

    end of ch 6

--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -6,7 +6,9 @@ defmodule Rumbl.Multimedia.Video do
     field :desscription, :string
     field :title, :string
     field :url, :string
+
     belongs_to :user, Rumbl.Accounts.User #connects videos to User schema
+    belongs_to :category, Rumbl.Multimedia.Category
 
     timestamps()
   end
@@ -15,7 +17,7 @@ defmodule Rumbl.Multimedia.Video do
   def changeset(video, attrs) do
     video
     #below shoujld be description but too lazy
-    |> cast(attrs, [:url, :title, :desscription]) #which fields from user-input may be allowed in
+    |> cast(attrs, [:url, :title, :desscription, :category_id]) #which fields from user-input may be allowed in
     |> validate_required([:url, :title, :desscription]) # validation of fields that MUST be present
   end
 end
