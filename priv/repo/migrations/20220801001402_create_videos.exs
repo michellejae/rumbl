1c2dd315527c786909738880d7f59eae273f4d46
michelle jae <moyer.mj@gmail.com>
2023-02-05 16:55:03 -0500

    asdf

--- priv/repo/migrations/20220801001402_create_videos.exs
+++ priv/repo/migrations/20220801001402_create_videos.exs
@@ -5,8 +5,9 @@ defmodule Rumbl.Repo.Migrations.CreateVideos do
     create table(:videos) do
       add :url, :string
       add :title, :string
-      add :desscription, :text
-      add :user_id, references(:users, on_delete: :nothing)
+      add :description, :text
+      add :user_id, references(:users)
+      #videos_userid_id_fkey
 
       timestamps()
     end
